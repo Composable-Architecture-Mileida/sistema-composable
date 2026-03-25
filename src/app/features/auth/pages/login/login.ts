cbc9d6298b8ce968311fa292f75298fd2ae876ba
mileywily <mileida.blanco.y@gmail.com>
2026-03-25 19:00:06 -0300

    feat(auth): implement loading state during login process

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -5,12 +5,13 @@ import { BaseInputComponent } from '../../../../shared/ui-kit/base-input/base-in
 import { BaseButtonComponent } from '../../../../shared/ui-kit/base-button/base-button';
 import { BaseNotificationComponent } from '../../../../shared/ui-kit/base-notification/base-notification';
 import { BaseModalComponent } from '../../../../shared/ui-kit/base-modal/base-modal';
+import { BaseLoaderComponent } from '../../../../shared/ui-kit/base-loader/base-loader';
 import { AuthService } from '../../../../core/services/auth';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, BaseInputComponent, BaseButtonComponent , BaseNotificationComponent, BaseModalComponent],
+  imports: [CommonModule, BaseInputComponent, BaseButtonComponent , BaseNotificationComponent, BaseModalComponent, BaseLoaderComponent],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
@@ -28,27 +29,40 @@ export class LoginComponent {
 
   showConfirmModal = signal(false);
 
+  isLoading = signal(false); // Nuevo Signal de carga
+
   onEmailChange(val: string) { this.email.set(val); }
   onPasswordChange(val: string) { this.password.set(val); }
 
   handleLogin() {
-    const success = this.authService.login(this.email(), this.password());
-    
-    if (success) 
-    {
-      // Si el login es exitoso, mandamos al usuario a una ruta interna
-      // Por ahora la redirigiremos a una que crearemos luego
-      this.feedbackType.set('success');
-      this.feedbackMessage.set('¡Bienvenida, ' + this.authService.currentUser().name + '!');
-      // this.router.navigate(['/dashboard']); 
-    }
-    else
-    { 
-      this.feedbackType.set('error');
-      this.feedbackMessage.set('Credenciales inválidas.Por favor, completa los campos');
-      this.showConfirmModal.set(true); // Abrimos el modal genérico
+    this.isLoading.set(true); // Encendemos el loader
 
-    }
+    // Simulamos una espera de red (2 segundos)
+    setTimeout(() => {
+        const success = this.authService.login(this.email(), this.password());
+        this.isLoading.set(false); // Apagamos el loader
+        if (!success) {
+        this.feedbackMessage.set('Credenciales incorrectas');
+        this.feedbackType.set('error');
+      }
+      /*   if (success) 
+        {
+          // Si el login es exitoso, mandamos al usuario a una ruta interna
+          // Por ahora la redirigiremos a una que crearemos luego
+          this.feedbackType.set('success');
+          this.feedbackMessage.set('¡Bienvenida, ' + this.authService.currentUser().name + '!');
+          // this.router.navigate(['/dashboard']); 
+        }
+        else
+        { 
+          this.feedbackType.set('error');
+          this.feedbackMessage.set('Credenciales inválidas.Por favor, completa los campos');
+          this.showConfirmModal.set(true); // Abrimos el modal genérico
+
+        } */
+      }, 2000);
+
+
   }
 
   processReset()
