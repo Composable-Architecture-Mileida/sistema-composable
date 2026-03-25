91136ac825fd2eb5e4fbc6a5c91bac57003b6ed5
mileywily <mileida.blanco.y@gmail.com>
2026-03-25 16:38:19 -0300

    refactor(shared): implement design tokens and separate scss files

--- src/app/shared/ui-kit/base-button/base-button.ts
+++ src/app/shared/ui-kit/base-button/base-button.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-base-button',
-  imports: [],
-  templateUrl: './base-button.html',
-  styleUrl: './base-button.scss',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './base-button.component.html',
+  styleUrl: './base-button.component.scss' // <-- Vinculado al archivo externo
 })
-export class BaseButton {}
+export class BaseButtonComponent {
+  variant = input<'primary' | 'outline'>('primary');
+  disabled = input<boolean>(false);
+  type = input<'button' | 'submit'>('button');
+  
+  onClick = output<MouseEvent>();
+}
