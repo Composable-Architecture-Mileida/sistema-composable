6576450fde7374c7edb38700f2030627cbe134e1
mileywily <mileida.blanco.y@gmail.com>
2026-03-25 17:55:49 -0300

    feat(auth): replace alerts with professional base-notification component

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -3,18 +3,22 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router'; // Importa el Router
 import { BaseInputComponent } from '../../../../shared/ui-kit/base-input/base-input';
 import { BaseButtonComponent } from '../../../../shared/ui-kit/base-button/base-button';
+import { BaseNotificationComponent } from '../../../../shared/ui-kit/base-notification/base-notification';
 import { AuthService } from '../../../../core/services/auth';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, BaseInputComponent, BaseButtonComponent],
+  imports: [CommonModule, BaseInputComponent, BaseButtonComponent , BaseNotificationComponent],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class LoginComponent {
   private authService = inject(AuthService);
   private router = inject(Router); // Inyectamos el router para navegar
+  // Signals para manejar el mensaje de la UI
+  feedbackMessage = signal<string>('');
+  feedbackType = signal<'success' | 'error'>('success');
   
   email = signal('');
   password = signal('');
@@ -25,13 +29,18 @@ export class LoginComponent {
   handleLogin() {
     const success = this.authService.login(this.email(), this.password());
     
-    if (success) {
+    if (success) 
+    {
       // Si el login es exitoso, mandamos al usuario a una ruta interna
       // Por ahora la redirigiremos a una que crearemos luego
-      alert('¡Bienvenida, ' + this.authService.currentUser().name + '!');
+      this.feedbackType.set('success');
+      this.feedbackMessage.set('¡Bienvenida, ' + this.authService.currentUser().name + '!');
       // this.router.navigate(['/dashboard']); 
-    } else {
-      alert('Por favor, completa los campos');
+    }
+    else
+    { 
+      this.feedbackType.set('error');
+      this.feedbackMessage.set('Credenciales inválidas.Por favor, completa los campos');
     }
   }
 }
