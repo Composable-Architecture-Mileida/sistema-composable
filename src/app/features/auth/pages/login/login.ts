df21c37bab155bb27f7f5039b7830e51f8c319ae
mileywily <mileida.blanco.y@gmail.com>
2026-03-25 17:01:54 -0300

    feat(auth): assemble login page using shared ui-kit components

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BaseInputComponent } from '../../../../shared/ui-kit/base-input/base-input';
+import { BaseButtonComponent } from '../../../../shared/ui-kit/base-button/base-button';
+import { AuthService } from '../../../../core/services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, BaseInputComponent, BaseButtonComponent],
   templateUrl: './login.html',
-  styleUrl: './login.scss',
+  styleUrl: './login.scss'
 })
-export class LoginComponent {}
+export class LoginComponent {
+  private authService = inject(AuthService);
+  
+  email = signal('');
+  password = signal('');
+
+  onEmailChange(val: string) { this.email.set(val); }
+  onPasswordChange(val: string) { this.password.set(val); }
+
+  handleLogin() {
+    console.log('Intentando login con:', this.email(), this.password());
+    // Aquí conectarás luego con tu microservicio de Node.js
+  }
+}
