b992d113d2ca592c1397856c64c7bd75f6e441d2
mileywily <mileida.blanco.y@gmail.com>
2026-04-08 15:52:03 -0400

    fix(auth-backend): configurar host 0.0.0.0 para acceso desde Docker

--- sistema-composable-backend/auth-microservicio/src/main.ts
+++ sistema-composable-backend/auth-microservicio/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// sistema-composable-backend/auth-microservicio/src/main.ts
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  // Habilitamos CORS para que tu Angular (puerto 4200) pueda hablar con NestJS (puerto 3000)
+  app.enableCors();
+
+  // Escuchamos en el puerto definido o el 3000, y forzamos el host 0.0.0.0
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
+  
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
