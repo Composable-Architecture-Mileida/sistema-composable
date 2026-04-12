58c25ba2fc32c3662eec968b41cad8e4ff882328
mileywily <mileida.blanco.y@gmail.com>
2026-04-12 19:32:44 -0400

    refactor(auth): #2 remove boilerplate and validate domain layer

--- sistema-composable-backend/auth-microservicio/src/app.module.ts
+++ sistema-composable-backend/auth-microservicio/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+//import { AppController } from './app.controller';
 import { AuthModule } from './domains/auth/auth.module';
 
 @Module({
   imports: [AuthModule],
-  controllers: [AppController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
---
+++ sistema-composable-backend/auth-microservicio/src/auth/domain/entities/user.entity.spec.ts
@@ -0,0 +1,9 @@
+import { User } from './user.entity';
+
+describe('User Entity', () => {
+  it('should create a user instance', () => {
+    const user = new User('1', 'test@test.com', 'Mileida');
+    expect(user.email).toBe('test@test.com');
+    expect(user.id).toBe('1');
+  });
+});
